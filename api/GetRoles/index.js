c6a1947142a59834d32cecd9874e8fc67919b876
Di Lin <dilin@microsoft.com>
2022-03-01 16:18:05 +0800

    Update index.js with group ids

--- api/GetRoles/index.js
+++ api/GetRoles/index.js
@@ -2,8 +2,8 @@ const fetch = require('node-fetch').default;
 
 // add role names to this object to map them to group ids in your AAD tenant
 const roleGroupMappings = {
-    'admin': '21a96550-aa02-486e-9297-e6e51b6398fc',
-    'reader': '33bb071c-118d-40d1-a5d7-7ced5900b973'
+    'admin': '997c67da-4c66-485e-8925-97e405ccd501',
+    'reader': '997c67da-4c66-485e-8925-97e405ccd501'
 };
 
 module.exports = async function (context, req) {
